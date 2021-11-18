e110f6e05982c6054250c20d87cacbdde42eb058
TANIA BAJO GARCIA <UO277084@uniovi.es>
2021-11-18 12:01:15 +0100

    Añadida clase Conejo

---
+++ NavesL4/Conejo.cpp
@@ -0,0 +1,70 @@
+#include "Conejo.h"
+
+Conejo::Conejo(float x, float y, Game* game)
+	: Actor("res/conejo.png", x, y, 36, 40, game) {
+
+	state = game->stateMoving;
+
+	lifes = 3;
+
+	aMoving = new Animation("res/enemigo_movimiento.png", width, height,
+		108, 40, 6, 3, true, game);
+	animation = aMoving;
+
+	vx = 1;
+	vxIntelligence = -1;
+	vx = vxIntelligence;
+
+}
+
+void Conejo::update() {
+	// Actualizar la animación
+	bool endAnimation = animation->update();
+
+	if (state == game->stateDying) {
+		state = game->stateDead;
+	}
+
+	// Establecer velocidad
+	if (state != game->stateDying) {
+		// no está muerto y se ha quedado parado
+		if (vx == 0) {
+			vxIntelligence = vxIntelligence * -1;
+			vx = vxIntelligence;
+		}
+		if (outRight) {
+			// mover hacia la izquierda vx tiene que ser negativa
+			if (vxIntelligence > 0) {
+				vxIntelligence = vxIntelligence * -1;
+			}
+			vx = vxIntelligence;
+		}
+		if (outLeft) {
+			// mover hacia la derecha vx tiene que ser positiva
+			if (vxIntelligence < 0) {
+				vxIntelligence = vxIntelligence * -1;
+			}
+			vx = vxIntelligence;
+		}
+
+	}
+	else {
+		vx = 0;
+	}
+
+
+
+}
+
+void Conejo::impacted() {
+	if (lifes == 1 &&state != game->stateDying) {
+		state = game->stateDying;
+	}
+	else {
+		lifes--;
+	}
+}
+
+void Conejo::draw(float scrollX, float scrollY) {
+	animation->draw(x - scrollX, y - scrollY);
+}
--- NavesL4/Conejo.h
+++ NavesL4/Conejo.h
@@ -3,15 +3,17 @@
 #include "Actor.h"
 #include "Animation.h" 
 
-class Abeja : public Actor
+class Conejo : public Actor
 {
 public:
-	Abeja(float x, float y, Game* game);
+	Conejo(float x, float y, Game* game);
 	void draw(float scrollX = 0, float scrollY = 0) override; // Va a sobrescribir
 	void update();
 	void impacted(); // Recibe impacto y pone animación de morir
-	float vyIntelligence;
+	float vxIntelligence;
 	int state;
+	int lifes;
+	int orientation; //0 -> izquierda y 1 -> derecha
 	Animation* aMoving;
 	Animation* animation; // Referencia a la animación mostrada
 };
